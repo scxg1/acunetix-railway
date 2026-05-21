cae7ce7e6b5697aacef1dbb62a17b296f2462282
samer <sc.xg.io@gmail.com>
2026-05-21 05:45:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
-
-# تشغيل Acunetix مباشرة بدون systemctl
-sudo -u acunetix /home/acunetix/.acunetix/supervisor -c /home/acunetix/.acunetix/supervisor.yaml &
-
-# تشغيل ttyd في الخلفية
-ttyd -p 7681 bash &
-
-# إبقاء الـ container شغّال
+ttyd -p 8080 bash &
 tail -f /dev/null
