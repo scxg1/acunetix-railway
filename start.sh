621fe47b5fb0109652c67e8bc9ed4800887da06a
samer <sc.xg.io@gmail.com>
2026-05-21 05:41:33 +0300

    Modify start.sh to enhance service startup
    Updated the start script to run Acunetix using supervisor and changed ttyd port.

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# إضافة hostname لـ /etc/hosts
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
-# تشغيل Acunetix
-sudo -u acunetix /home/acunetix/.acunetix/acunetix start &
+# تشغيل Acunetix مباشرة بدون systemctl
+sudo -u acunetix /home/acunetix/.acunetix/supervisor -c /home/acunetix/.acunetix/supervisor.yaml &
 
-# تشغيل ttyd
-ttyd -p 8080 bash
+# تشغيل ttyd في الخلفية
+ttyd -p 7681 bash &
+
+# إبقاء الـ container شغّال
+tail -f /dev/null
